5c99d30d96042e73bad198331bd0ff75982f1195
Nicolas <X00188279@mytublin.ie>
2022-10-28 11:52:24 +0100

    lab9 part 1 commit

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from .views import SignUpView
+
+from accounts.models import Profile
+from .views import SignUpView, ProfileEditView, ProfilePageView
 
 urlpatterns=[
     path('signup/', SignUpView.as_view(), name='signup'),
+    path('edit_profile/<int:pk>/', ProfileEditView.as_view(), name='edit_profile'),
+    path('profile/<int:pk>/', ProfilePageView.as_view(), name='show_profile')
 ]
