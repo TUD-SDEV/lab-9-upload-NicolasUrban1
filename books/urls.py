83d98564d2a843621e65307dce3a6513cd25911b
Nicolas <X00188279@mytublin.ie>
2022-11-10 13:06:08 +0000

    lab9 part 4

--- books/urls.py
+++ books/urls.py
@@ -1,7 +1,7 @@
-from django.urls import URLPattern, path
-from .views import BookListView, BookDetailView, BookCreateView, BookUpdateView, BookDeleteView
+from django.urls import path
+from .views import BookListView, BookDetailView
 
-URLPatterns = [
+urlpatterns = [
     path('', BookListView.as_view(), name='book_list'),
     path('<uuid:pk>', BookDetailView.as_view(), name='book_detail'),
 ]
--- books/views.py
+++ books/views.py
@@ -5,6 +5,7 @@ class BookListView(ListView):
     model = Book
     context_object_name = 'book_list'
     template_name = 'books/book_list.html'
+    
 class BookDetailView(DetailView):
     model = Book
     template_name = 'books/book_detail.html'
